05cb124c4290714e89c2efea13df26478231e03a
karenMora <karen.mora@mail.escuelaing.edu.co>
2018-12-14 09:26:56 -0500

    taller inicial

---
+++ src/main/java/taller/ARSW/ArswApplication.java
@@ -0,0 +1,14 @@
+package taller.ARSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArswApplication.class, args);
+	}
+
+}
+
